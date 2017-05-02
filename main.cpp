5d4ad110df6e3e4fcb2ed2df6bfdf850c75d6723
Dominik <dominikb94@gmail.com>
2017-05-02 09:22:11 +0200

    Pridane do gitu

--- main.cpp
+++ main.cpp
@@ -1,50 +1,81 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct highway {
-    int city1;
-    int city2;
-    int cost;
-};
-struct compare_cost{
-    bool operator() (const highway lhs, const highway rhs) const{
-        return lhs.cost < rhs.cost;
-    }
-};
-
 int n;
-vector<highway> costs;
+long long sum;
+vector<pair<int, pair<int, int>>> highways;
 
-int rob_co_mas() {
-    vector<int> cities;
-    sort(costs.begin(), costs.end(),compare_cost());
-    //pridam najlacnejsiu, bude v kostre
-    cities.push_back(costs.at(0).city1);
-    cities.push_back(costs.at(0).city2);
-    for(int i = 0; i < n-1; i++){
-        
+class set {
+public:
+    vector<int> members;
+    vector<int> set_size;
+
+    set(int n) {
+        for (int i = 0; i < n; i++) {
+            members.push_back(i);
+            set_size.push_back(1);
+        }
     }
-    return 0;
+
+    int shortest_path(int i) {
+        while (i != members.at(i)) {
+            members.at(i) = members.at(members.at(i));
+            i = members.at(i);
+        }
+        return i;
+    }
+
+    void join(int u, int v) {
+        int i = shortest_path(u);
+        int j = shortest_path(v);
+
+        if (set_size.at(i) < set_size.at(j)) {
+            members.at(i) = j;
+            set_size.at(j) += set_size.at(i);
+        } else {
+            members.at(j) = i;
+            set_size.at(i) += set_size.at(j);
+        }
+    }
+
+    int is_memeber(int u, int v) {
+        return shortest_path(u) == shortest_path(v);
+    }
+
+};
+
+void rob_co_mas() {
+    set graf(n);
+    int u, v;
+    sort(highways.begin(), highways.end());
+    int size = highways.size();
+    for (int i = 0; i < size; ++i) {
+        u = highways[i].second.first;
+        v = highways[i].second.second;
+        if (!graf.is_memeber(u, v)) {
+            graf.join(u, v);
+            sum += highways.at(i).first;
+        } else if(highways.at(i).first < 0){
+            sum += highways.at(i).first;
+        }
+    }
 }
 
-
 int main() {
+    int cost;
     cin >> n;
-
     for (int i = 0; i < n; i++) {
-        vector<highway> temp;
         for (int j = 0; j < n; j++) {
-            if (i != j) {
-                highway meh;
-                cin >> meh.cost;
-                meh.city1 = i;
-                meh.city2 = j;
-                costs.push_back(meh);
+            cin >> cost;
+            if (i < j) {
+                highways.push_back(pair<int, pair<int, int>>(cost, pair<int, int>(i, j)));
             }
         }
-
     }
+    rob_co_mas();
+    cout << sum << endl;
     return 0;
 }
